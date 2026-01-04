07038b09f1aa2c7af89c1dd58a099b47c479187c
Calvin Halim <cavim1998@gmail.com>
2026-01-04 15:46:39 +0700

    adjust navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,13 +41,6 @@ const Navbar = () => {
                   Register
                 </Button>
               </Link>
-              {isOrganizer && (
-                <Link href="/dashboard">
-                  <Button className="rounded-full border bg-white text-black hover:bg-gray-100">
-                    Dashboard
-                  </Button>
-                </Link>
-              )}
             </>
           ) : (
             <>
@@ -65,6 +58,13 @@ const Navbar = () => {
                 Logout
               </Button>
             </>
+          )}
+          {isOrganizer && (
+            <Link href="/dashboard">
+              <Button className="rounded-full border bg-white text-black hover:bg-gray-100">
+                Dashboard
+              </Button>
+            </Link>
           )}
         </div>
       </div>
